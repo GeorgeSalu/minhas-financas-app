f0858683caee680f583a6119fadf222d4a53df00
George Salu <george.salu10@gmail.com>
2020-03-03 17:11:02 -0300

    implementando o componente de cadastro de usuarios

---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
+
+class CadastroUsuario extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Card title="cadastro de usuario">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="bs-component">
+
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default CadastroUsuario

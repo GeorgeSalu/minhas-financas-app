bec7f1533e8a8c2277db4ddee8a6131594748cbc
George Salu <george.salu10@gmail.com>
2020-03-03 11:24:54 -0300

    instalando e testando o Bootswatch/Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css'
 
 class App extends React.Component {
-
-  state = {
-    numero1: null,
-    numero2: null,
-    resultado: null
-  }
-
-  somar = () => {
-    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({resultado: resultado})
-  }
-
   render() {
     return (
       <div>
-        <label>Nome: </label>
-        <input type="text" value={this.state.numero1} 
-          onChange={(e) => this.setState({numero1: e.target.value})} />
-
-        <input type="text" value={this.state.numero2} 
-          onChange={(e) => this.setState({numero2: e.target.value})} />
-
-        <button onClick={this.somar}>somar</button>
-
-        O nome digitado foi: {this.state.resultado}
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     )
   }

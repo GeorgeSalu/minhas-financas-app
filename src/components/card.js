3a4cb1c73834f392df5ce22e56f2537473f56154
George Salu <george.salu10@gmail.com>
2020-03-03 12:55:55 -0300

    criando os componentes de login e card

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card md-3">
+        <div className="card-header"></div>
+        <div className="card-body">
+          
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/views/login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+            <div className="bs-docs-section">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login

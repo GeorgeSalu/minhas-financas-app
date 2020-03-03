5cf764bb11efd4f87ef5dbe5a70237b3ffc3d2ac
George Salu <george.salu10@gmail.com>
2020-03-03 11:15:04 -0300

    trabalhando com estado e comportamento nos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    numero1: null,
+    numero2: null,
+    resultado: null
+  }
+
+  somar = () => {
+    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+    this.setState({resultado: resultado})
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Nome: </label>
+        <input type="text" value={this.state.numero1} 
+          onChange={(e) => this.setState({numero1: e.target.value})} />
+
+        <input type="text" value={this.state.numero2} 
+          onChange={(e) => this.setState({numero2: e.target.value})} />
+
+        <button onClick={this.somar}>somar</button>
+
+        O nome digitado foi: {this.state.resultado}
+      </div>
+    )
+  }
 }
 
 export default App;

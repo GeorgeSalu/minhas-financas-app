b9327ab95aa6a744a92e800ac065935104005200
George Salu <george.salu10@gmail.com>
2020-03-03 14:33:29 -0300

    componenteizando o formulario de login

--- src/views/login.js
+++ src/views/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
+import FromGrop from '../components/form-group'
 
 class Login extends React.Component {
   render() {
@@ -9,7 +10,22 @@ class Login extends React.Component {
           <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
             <div className="bs-docs-section">
               <Card title="login">
-                meu cartao
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="bs-component">
+                      <fieldset>
+                        <FromGrop label="Email" htmlFor="examlpleInputEmail">
+                          <input type="email" className="form-control" id="exampleInputEmail1"
+                               aria-describedby="emailHelp" placeholder="Digite o Email"/>
+                        </FromGrop>
+                        <FromGrop label="senha" htmlFor="">
+                          <input type="password" className="form-control" 
+                              id="exampleInputPassword1" placeholder="Password"/>
+                        </FromGrop>
+                      </fieldset>
+                    </div>
+                  </div>
+                </div>
               </Card>
             </div>
           </div>

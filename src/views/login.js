6aad4bcc4977f7cc15af514f030f34d34ce4692e
George Salu <george.salu10@gmail.com>
2020-03-15 14:22:43 -0300

    conectando o login com a API REST

--- src/views/login.js
+++ src/views/login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../components/card'
 import FromGrop from '../components/form-group'
 import {withRouter} from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends React.Component {
 
@@ -11,8 +12,17 @@ class Login extends React.Component {
   }
 
   entrar = () => {
-    console.log('email', this.state.email);
-    console.log('senha', this.state.senha);
+    axios
+      .post('http://localhost:8080/api/usuarios/autenticar', {
+        email: this.state.email,
+        senha: this.state.senha
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(erro => {
+        console.log(erro.response)
+      })
   }
 
   prepareCadastrar = () => {

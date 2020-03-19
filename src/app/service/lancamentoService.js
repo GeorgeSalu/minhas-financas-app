6cad4ed180b4470fc75ddf3802a19bdb5ccdfdf1
George Salu <george.salu10@gmail.com>
2020-03-18 21:40:28 -0300

    implementando o servico da api de lancamentos

---
+++ src/app/service/lancamentoService.js
@@ -0,0 +1,12 @@
+import ApiService from '../apiservice'
+
+export default class LancamentoService extends ApiService {
+
+  constructor() {
+    super('/api/lancamentos')
+  }
+
+  consultar(lancamento) {
+    
+  }
+}

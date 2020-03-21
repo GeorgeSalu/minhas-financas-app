32abc384fa6b03f2adae5628581349cb3706ad37
George Salu <george.salu10@gmail.com>
2020-03-21 12:13:39 -0300

    testando a lib primereact

--- src/main/App.js
+++ src/main/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Rotas from './rotas'
 import NavBar from '../components/navbar'
 
+import { Button } from 'primereact/button'
+
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.min.css'
 
@@ -9,11 +11,17 @@ import 'toastr/build/toastr.min.js'
 
 import 'toastr/build/toastr.css'
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+
 class App extends React.Component {
   render() {
     return (
       <>
       <NavBar />
+      <Button label="Click" icon="pi pi-check" iconPos="right"></Button>
       <div className="container">
         <Rotas />
       </div>

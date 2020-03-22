8148b073efacf6c80035153ceed90cd3ec367635
George Salu <george.salu10@gmail.com>
2020-03-21 22:06:11 -0300

    criando o formulario de cadastro e atualizacao de lancamentos

--- src/views/lancamentos/cadastro-lancamentos.js
+++ src/views/lancamentos/cadastro-lancamentos.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Card from '../../components/card'
 import FormGroup from '../../components/form-group'
+import SelectMenu from '../../components/selectMenu'
 import { withRouter } from 'react-router-dom'
+import LancamentoService from '../../app/service/lancamentoService'
 
 class CadastroLancamentos extends React.Component {
+  constructor() {
+    super()
+    this.service = new LancamentoService()
+  }
+
   render() {
+    const tipos = this.service.obterListaTipos()
+    const meses = this.service.obterListaMeses()
     return (
-      <Card>
+      <Card title="Cadastro Lancamento">
         <div className="row">
           <div className="col-md-12">
             <FormGroup id="inputDescricao" label="Descricao: ">
@@ -22,10 +31,27 @@ class CadastroLancamentos extends React.Component {
           </div>
           <div className="col-md-6">
             <FormGroup id="inputMes" label="Mes: ">
-              <input id="inputMes" type="text" className="form-control" ></input>
+              <SelectMenu id="inputMes" lista={meses} className="form-control" ></SelectMenu>
             </FormGroup>
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-4">
+            <FormGroup id="inputTipo" label="Tipo: ">
+              <SelectMenu id="inputTipo" lista={tipos} className="form-control" ></SelectMenu>
+            </FormGroup>
+          </div>
+          <div className="col-md-4">
+            <FormGroup id="inputStatus" label="Status: ">
+              <input type="text" className="form-control" disabled></input>
+            </FormGroup>
+          </div>
+        </div>
+        
+          <div className="row">
+            <button className="btn btn-success">Salvar</button>
+            <button className="btn btn-danger">Cancelar</button>
+          </div>
       </Card>
     )
   }

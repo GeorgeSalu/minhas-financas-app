294cf63c27d2669e6d5ddd426b90fa0d1794d22d
George Salu <george.salu10@gmail.com>
2020-03-25 19:08:27 -0300

    implementando o provedor de autenticacao

--- src/app/service/authService.js
+++ src/app/service/authService.js
@@ -1,12 +1,22 @@
 import LocalStorageService from './localstorageService'
 
+export const USUARIO_LOGADO = '_usuario_logado';
+
 export default class AuthService {
   static isUsuarioAutenticado() {
-    const usuario = LocalStorageService.obterItem('_usuario_logado')
+    const usuario = LocalStorageService.obterItem(USUARIO_LOGADO)
     return usuario && usuario.id; 
   }
 
   static removerUsuarioAutenticado() {
-    LocalStorageService.removerItem('_usuario_logado')
+    LocalStorageService.removerItem(USUARIO_LOGADO)
+  }
+
+  static logar(usuario) {
+    LocalStorageService.adicionarItem(USUARIO_LOGADO, usuario)
+  }
+
+  static obterUsuarioAutenticado() {
+    return LocalStorageService.obterItem(USUARIO_LOGADO)
   }
 }
---
+++ src/main/provedorAutenticacao.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import AuthService from '../app/service/authService'
+
+export const AuthContext = React.createContext()
+export const AuthConsumer = AuthContext.Consumer;
+const AuthProvider = AuthContext.Provider;
+
+class ProvedorAutenticacao extends React.Component {
+  state = {
+    usuarioAutenticado: null,
+    isAutenticado: false
+  }
+
+  iniciarSessao = (usuario) => {
+    AuthService.logar(usuario);
+    this.setState({ isAutenticado: true, usuarioAutenticado: usuario })
+  }
+
+  encerrarSessao = () => {
+    AuthService.removerUsuarioAutenticado();
+    this.setState({ isAutenticado: false, usuarioAutenticado: null })
+  }
+
+  render() {
+    return (
+      <AuthProvider >
+        {this.props.childrens}
+      </AuthProvider>
+    )
+  }
+}
